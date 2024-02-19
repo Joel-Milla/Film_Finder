dfb76e60b198bd00f1caa5b0896e40fb2e19c02a
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-02-19 12:54:04 -0600

    Create basic functions for the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,27 @@
 const tmdbKey = '8f9c86e8f505462dfb762e6bc1370816';
-const tmdbBaseUrl = "";
+const tmdbBaseUrl = "https://api.themoviedb.org/3";
+const playBtn = document.getElementById("platBtn");
+
+const getGenres = () => {
+
+};
+
+const getMovies = () => {
+    const selectedGenre = getSelectedGenre();
+};
+
+const getMovieInfo = () => {
+
+};
+
+// Get a list of movies and display the info of a random movie from the list
+const showRandomMovie = () => {
+    const movieInfo = document.getElementById("movieInfo");
+
+    if (movieInfo.childNodes.length > 0) {
+        clearCurrentMovie();
+    }
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.addEventListener("onclick", showRandomMovie);
