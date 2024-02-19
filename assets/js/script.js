a5ad9cd41b02841c45657b8c741a8a8235db65a4
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-02-19 13:31:31 -0600

    Add fetch request for genres of movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
 const tmdbKey = '8f9c86e8f505462dfb762e6bc1370816';
 const tmdbBaseUrl = "https://api.themoviedb.org/3";
-const playBtn = document.getElementById("platBtn");
+const playBtn = document.getElementById("playBtn");
 
-const getGenres = () => {
+const getGenres = async () => {
+    const genreRequestEndpoint = "/genre/movie/list";
+    const requestParams = `?api_key=${tmdbKey}`;
+    const urlToFetch = tmdbBaseUrl + genreRequestEndpoint + requestParams;
 
+    // Make request to the endpoint
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+
+            const genres = jsonResponse.genres;
+            return genres;
+        }
+        throw new Error("Error while fetching genres")
+    } catch(error) {
+        console.log("Error while fetching genres ", error)
+    }
 };
 
 const getMovies = () => {
@@ -24,4 +41,4 @@ const showRandomMovie = () => {
 };
 
 getGenres().then(populateGenreDropdown);
-playBtn.addEventListener("onclick", showRandomMovie);
+playBtn.addEventListener("click", showRandomMovie);
