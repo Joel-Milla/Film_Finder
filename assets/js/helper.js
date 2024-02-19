13fb17ced81332a0714ff8f48d49846c6acd268b
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-02-19 12:43:04 -0600

    Create functions of javascript to read and manage the DOM

---
+++ assets/js/helper.js
@@ -0,0 +1,101 @@
+// Populate dropdown menus with available genres
+const populateGenreDropdown = (genres) => {
+    const select = document.getElementById("genres");
+
+    for (const genre of genres) {
+        let option = document.createElement("option");
+        option.value = genre.id;
+        option.text = genre.name;
+        select.appendChild(option);
+    }
+};
+
+// Returns genre selection from dropdown menu
+const getSelectedGenre = () => {
+    const getSelectedGenre = document.getElementById("genres").value;
+    return getSelectedGenre;
+};
+
+// Displays the likes and dislikes elements
+const showBtns = () => {
+    const btnsDiv = document.getElementById("likeOrDislikeBtns");
+    btnsDiv.removeAttribute("hidden")
+};
+
+// Clear movie from screen
+const clearCurrentMovie = () => {
+    const moviePosterDiv = document.getElementById("moviePoster");
+    const movieTextDiv = document.getElementById("movieText");
+    moviePosterDiv.innerHTML = "";
+    movieTextDiv.innerHTML = "";
+};
+
+// After liking movie, clear current movie and get another one
+const likeMovie = () => {
+    clearCurrentMovie();
+    showRandomMovie();
+};
+
+// After disliking movie, clear current movie and get another one
+const dislikeMovie = () => {
+    clearCurrentMovie();
+    showRandomMovie();
+};
+
+// Create HTML for movie poster
+const createMoviePoster = (posterPath) => {
+    const moviePosterUrl = `https://image.tmdb.org/t/p/original/${posterPath}`;
+
+    const posterImg = document.createElement("img");
+    posterImg.setAttribute("src", moviePosterUrl);
+    posterImg.setAttribute("id", "moviePoster");
+
+    return posterImg;
+};
+
+// Create HTML for movie title
+const createMovieTitle = (title) => {
+    const movieTitle = document.createElement("h1");
+    movieTitle.setAttribute("id", "movieTitle");
+    movieTitle.innerHTML = title;
+
+    return movieTitle;
+};
+
+// Create HTML for movie overview
+const createMovieOverview = (overview) => {
+    const movieOverview = document.createElement("p");
+    movieOverview.setAttribute("id", "movieOverview");
+    movieOverview.innerHTML = overview;
+
+    return movieOverview;
+}
+
+// Return random movie from first page movies
+const getRandomMovie = (movies) => {
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    const randomMovie = movies[randomIndex];
+
+    return randomMovie;
+};
+
+// Uses the DOM to create HTML to display movie
+const displayMovie = (movieInfo) => {
+    const moviePosterDiv = document.getElementById("moviePoster");
+    const movieTextDiv = document.getElementById("movieText");
+    const likeBtn = document.getElementById("likeBtn");
+    const dislikeBtn = document.getElementById("dislikeBtn");
+
+    // Create HTML content using movie info
+    const moviePoster = createMoviePoster(movieInfo.poster_path);
+    const titleHeader = createMovieTitle(movieInfo.title);
+    const overviewText = createMovieOverview(movieInfo.overview);
+
+    moviePosterDiv.appendChild(moviePoster);
+    movieTextDiv.appendChild(titleHeader);
+    movieTextDiv.appendChild(overviewText);
+
+    showBtns();
+    likeBtn.addEventListener("click", likeMovie);
+    dislikeBtn.addEventListener("click", likeMovie);
+}
